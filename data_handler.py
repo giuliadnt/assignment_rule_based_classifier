2de765a15c8796f56a53e1a4c1de8a010cd6931a
giulia dnt <giulia.dnt@gmail.com>
2022-01-19 21:10:48 +0100

    Docstrings =

--- data_handler.py
+++ data_handler.py
@@ -9,17 +9,35 @@ class DataHandler:
         self.df = pd.read_csv(self.filepath)
 
     def clean_df(self):
+        """
+        basic preprocessing applied to dataframe's text column
+        (removal or replacement of special chars, punctuation, multiple spaces)
+        :return: None
+        """
         self.df['text'] = self.df['text'].replace({r'\s\s+': ' ', '\n': '', '[,().:;#]': '', ' - ': ' ', '- ': ' '}, regex=True)
         self.df['text'] = self.df['text'].str.strip()
         self.df['text'] = self.df['text'].str.lower()
+
         # return self.df[self.df['text'].apply(lambda x: len(x.split()) > 1)]
 
     def get_classes_sets(self, status_value):
+        """
+        Method used for exploratory analysys
+        :param status_value: one of the df classes (Non Smoker, Smoker, Former Smoker, Unknown)
+        :return: list of all text items in a specific class
+        """
         filtered_df = self.df.loc[self.df['status'] == status_value]
         return filtered_df['text'].tolist()
 
     @staticmethod
     def get_most_frequent_tokens_per_class(filtered_df_list):
+        """
+        Method used for exploratory analysys
+        returns dict in descending order (most frequent tokens first)
+        stopwords are removed
+        :param filtered_df_list:
+        :return: frequency dict
+        """
         stopwords = ['of', 'to', 'and', 'in', 'at', 'by', 'with', 'for', 'a', 'the', '']
         tokens = [record.split() for record in filtered_df_list]
         bag_of_words = [token for sent in tokens for token in sent]
@@ -27,10 +45,15 @@ class DataHandler:
         return Counter(no_stopwords)
 
     def split_testsets(self):
-        X = self.df['text']
-        y = self.df['status']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=5)
-        return X_train, X_test, y_train, y_test
+        """
+        Splits of original df in train and test (to verify results on unseen data)
+        :return:
+        """
+        # X = self.df['text']
+        # y = self.df['status']
+        #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=5)
+        train, test  = train_test_split(self.df, test_size=0.3, random_state=5)
+        return train, test
 
 
 
--- runner.py
+++ runner.py
@@ -5,22 +5,34 @@ from sklearn.metrics import classification_report
 
 
 def write_output(res, set_name):
+    """
+    Writes to csv the is and the predicted status
+    :param res: RuleClassifier results (list of tuples)
+    :param set_name: string (train or test)
+    """
     with open(f'results_{set_name}.csv', 'w') as f:
         csv_out = csv.writer(f)
-        csv_out.writerow(['status', 'text'])
+        csv_out.writerow(['id', 'status'])
         for row in res:
             csv_out.writerow(row)
 
 
-def run_classifier(split_dataset, true_labels):
+def run_classifier(split_dataset):
+    """
+    Performs classification and outputs metrics report
+    :param split_dataset: dataframe (train or test)
+    :return: list of tuples (id, status)
+    """
+    true_labels = split_dataset['status'].tolist()
     predicted_labels = []
     rc = RuleClassifier(split_dataset, kb_path='kb.json')
 
-    for text in rc.data:
-        rc.classify(text)
+    for index, row in rc.data.iterrows():
+        rc.classify(row['text'], row['row_id'])
 
     for result in rc.res:
-        predicted_labels.append(result[0])
+
+        predicted_labels.append(result[1])
 
     print(classification_report(true_labels, predicted_labels,
                                 target_names=['Smoker', 'Non Smoker', 'Unknown', 'Former Smoker']))
@@ -31,14 +43,14 @@ def run_classifier(split_dataset, true_labels):
 def main():
     handler = DataHandler(filepath='dataset/smoker_status.csv')
     handler.clean_df()
-    X_train, X_test, y_train, y_test = handler.split_testsets()
+    train, test = handler.split_testsets()
 
     print("RUN ON TRAIN \n")
-    results_train = run_classifier(X_train, y_train)
+    results_train = run_classifier(train)
     write_output(results_train, 'train')
 
     print("RUN ON TEST \n")
-    results_test = run_classifier(X_test, y_test)
+    results_test = run_classifier(test)
     write_output(results_test, 'test')
 
 
